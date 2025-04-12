5efc00d4642f5b9da1bc1f0306ec91b131805dea
willyan santos tomaz e silva <willyantomaz2015@gmail.com>
2025-04-12 17:25:50 -0300

    spring initialise

---
+++ src/main/java/br/com/desafioP/DpCrud/DpCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafioP.DpCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpCrudApplication.class, args);
+	}
+
+}
